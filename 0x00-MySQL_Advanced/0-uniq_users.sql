0b66fd24a65bb44f9afee9e290bdc15d1aa2cd7a
Anthony <ndegwaanthony300@gmail.com>
2024-07-12 00:32:32 +0300

    initial commit

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
--- script that create table users
-
+-- creates table user
 CREATE TABLE IF NOT EXISTS users(
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255)
 )
