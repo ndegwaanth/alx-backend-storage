c501fddb7821e595f6eaf5b5d43e8cc8652b9163
Anthony <ndegwaanthony300@gmail.com>
2024-07-16 23:19:39 +0300

    0-list_databases

---
+++ 0x01-NoSQL/0x02-test/test.py
@@ -0,0 +1,46 @@
+from pymongo import MongoClient
+import json
+
+link = "mongodb+srv://ndegwaanth:bsitc01@cluster0.9eytvzr.mongodb.net/Database1?retryWrites=true&w=majority&appName=Cluster0"
+# cluster = "mongodb+srv://ndegwaanth:<bsitc01/0945/2021>@cluster0.crseyiw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(link)
+
+print(client.list_database_names())
+
+db = client.Database1
+print(db.list_collection_names())
+
+todo1 = [
+    {
+        'age': 37,
+        'fullnames': 'Mary Onyango',
+        'city': [ 'Chicago' ],
+        'hobbies': [ 'Programming', 'Reading' ]
+    },
+    {
+        'name': 'Mike Smith',
+        'age': 35,
+        'city': 'San Francisco'
+    },
+    {
+        'name': 'Anthony',
+        'Address': { 'street': 'USA' },
+        'hobbies': [ 'Codding' ]
+    }
+]
+
+todos = db.todos
+test = db.test
+todos.insert_many(todo1)
+test.insert_many(todo1)
+
+print(test.find_one({'fullnames':'Mary Onyango', 'age': 37}))
+# print(test.find_many())
+
+data = test.find()
+
+# for i in data:
+#     print((i))
+
+print(test.count_documents({}))
