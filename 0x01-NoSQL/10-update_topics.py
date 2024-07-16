8bb1fbb0c6924d38422dcb5a1eea6c762b070327
Anthony <ndegwaanthony300@gmail.com>
2024-07-17 01:10:17 +0300

    10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,12 +2,15 @@
 """ Change school topics"""
 
 
-def update_topic(mongo_collection, name, topics):
-    """"
-    changes all topics of a school document
-    :param:mongo_collection: database collection
-    :param:name: (string) will be school name to update
-    :param:topics: list of topics approached in the school
+def update_topics(mongo_collection, name, topics):
     """
-    mongo_collection.update_many({
-        "name": name}, {"topics": topics})
+    Changes all topics of a school document based on the name.
+
+    :param mongo_collection: pymongo collection object
+    :param name: string, school name to update
+    :param topics: list of strings, list of topics approached in the school
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
