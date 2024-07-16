6a1c9b56d33db71500d9a70c6c4dcef2da76c16b
Anthony <ndegwaanthony300@gmail.com>
2024-07-17 01:20:31 +0300

    11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -2,11 +2,12 @@
 """Where can I learn Python?"""
 
 
-def school_by_topic(mongo_collection, topic):
+def schools_by_topic(mongo_collection, topic):
     """
-    return the list of school having specific topic
-    :param:mongo_collection:database collection
-    :param:topic:(string) will be topic searched
-    :return: list of school
+    Returns a list of schools having a specific topic.
+
+    :param mongo_collection: pymongo collection object
+    :param topic: string, topic searched
+    :return: list of schools matching the topic
     """
     return list(mongo_collection.find({"topics": topic}))
